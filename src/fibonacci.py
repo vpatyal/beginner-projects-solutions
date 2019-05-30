31cfbf9f1dbe897164f228a02a5336a8c673d034
vpatyal <vinod.patyal@rhsmith.umd.edu>
2019-05-29 21:32:15 -0400

    Fibonacci sequence code - updated

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -4,18 +4,19 @@ import sys
 def calculate_fibonacci_number(n):
 	# initialize the fibonacci sequence with first 2 numbers
 	fibonacci_seq = [0, 1]
-	for x in range(n - 2):
-		xplus2_number = fibonacci_seq[x] + fibonacci_seq[x + 1]
-		# print("{}, {}, {}, {}".format(x, xplus2_number, fibonacci_seq[x], fibonacci_seq[x - 1]))
-		fibonacci_seq.append(xplus2_number)
 
-	print("Fibonacci number at {0}th sequence is: {1}".format(n, fibonacci_seq[n - 1]))
-	print("Fibonacci sequence :" + str(fibonacci_seq[0:n]))
+	# calculate fibonacci number at x+2 location in the fibonacci sequence
+	for x in range(n):
+		fibonacci_number_at_xplus2 = fibonacci_seq[x] + fibonacci_seq[x + 1]
+		fibonacci_seq.append(fibonacci_number_at_xplus2)
+
+	print("Fibonacci number at nth sequence is: {}".format(fibonacci_seq[n]))
+	print("Fibonacci sequence :" + str(fibonacci_seq[0:n+1]))
 
 
 def main():
 	try:
-		n = int(input("Enter a number to get fibonnaci number at that sequence: "))
+		n = int(input("Enter a number n to get fibonnaci number at that sequence: "))
 	except (ValueError):
 		print("Not a valid number, program terminating!")
 		sys.exit(99)
